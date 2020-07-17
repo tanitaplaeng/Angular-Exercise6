8525584871606cb1a93844f04450cb8df67d8132
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-07-16 21:03:19 -0400

    updated dog/cat components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/dog', pathMatch: 'full'},
+      {path: 'dog', component: DogComponent},
+      {path: 'cat', component: CatComponent},
+    ]),
+    HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
